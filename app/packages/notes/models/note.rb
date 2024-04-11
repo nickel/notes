5a8f88189a3ef8578577ab31e676fe17ec7fcb02
Juan Gallego IV <juan@gallegoiv.com>
2024-04-11 22:35:33 +0200

    Summary of changes: Updated code and added functionalities to notes - Reduced margin-bottom in notes.css for improved visual flow - Split note into public and private into notes_controller.rb for better note categorization - Introduced 'intro' function in note.rb to format content to display a introduction summary - Sorted notes by date created instead of updated for more relevant ordering in note/find_all.rb - Added create and delete note functionalities in _form.html.erb and _note.html.erb for added user interactivity - Grouped tags in tag/find_all.rb allowing users to browse by tags - Added date next to note title and a brief intro of the note in index.html.erb for better understanding of each note - Updated test suit in find_all_test.rb to test new tags are grouped functionality

--- app/packages/notes/models/note.rb
+++ app/packages/notes/models/note.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
 class Note < ApplicationRecord
+  def intro
+    return "" unless content.present?
+
+    content
+      .split("\n")
+      .first
+  end
+
   def to_struct
-    CustomStruct.new(attributes)
+    CustomStruct.new(
+      attributes
+        .merge(intro:)
+    )
   end
 
   def to_param
